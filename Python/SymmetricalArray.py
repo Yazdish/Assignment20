768b0f19985acb6794156ea62875153b3966b50b
Yazdan Shouryabi <yazdishouryabi@gmail.com>
2024-02-23 00:12:21 +0330

    Symmetrical Array

--- Python/SymmetricalArray.py
+++ Python/SymmetricalArray.py
@@ -1,15 +1,25 @@
-import random
 
-boys = ["mohammad", "sobhan", "abdollah", "kiya", "mahdi", "sajjad", "homan", "arman"]
-girls = ["mahtab", "hane", "harir", "fateme", "kiana", "faezeh", "minoo", "mina", "soghra"]
 
-result = []
+print (" Please enter your array's numbers one by one as inputs, and when you are finished print 'done' as an input ")
 
-for i in range(min(len(boys),len(girls))): 
-    groom = random.choice(boys)
-    bride = random.choice(girls)
-    result.append([groom,bride])
-    boys.remove(groom)
-    girls.remove(bride)
+array = []
 
-print("Couples: ", result)
+while True :
+    user_input = input (" Please enter your input :")
+    if user_input == "done" :
+        break
+
+    else :
+        sentence = int (user_input)
+        array.append ( sentence )
+
+print ("Your array is : ")
+print ( array )
+
+for i in range (len(array)//2) :
+    if array[i] != array[len(array) - 1 - i] :
+        print (" This array is NOT SYMMETRIC")
+        break
+
+else :
+    print (" This array is SYMMETRIC")
